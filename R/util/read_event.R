bf149cdafd2b7df526d8118275739c249d0ea70f
Dan Negrey <dnegrey@gmail.com>
2025-03-24 09:47:48 -0400

    Added game scores to read_event

--- R/util/read_event.R
+++ R/util/read_event.R
@@ -8,5 +8,25 @@ read_event <- function(f) {
     loc_short = x$loc_short
   )
   y$members <- names(x)[!(names(x) %in% names(y))]
+  games <- integer()
+  for (mem in y$members) {
+    games <- c(games, as.integer(names(x[[mem]])))
+  }
+  y$num_games <- max(games)
+  rm(mem, games)
+  for (m in y$members) {
+    for (g in 1:y$num_games) {
+      tmp <- transpose_score(score_game(x[[m]][[g]]))
+      tmp <- cbind(
+        data.frame(Member = m, Game = g, tmp)
+      )
+      if (!("scores" %in% ls())) {
+        scores <- tmp
+      } else {
+        scores <- rbind(scores, tmp)
+      }
+    }
+  }
+  y$scores <- scores
   return(y)
 }
